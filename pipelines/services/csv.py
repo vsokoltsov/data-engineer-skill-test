ef06267809515001642864b2344126e5a313f4bb
Vadim Sokoltsov <vforvad@gmail.com>
2025-12-29 23:44:46 +0100

    Replace direction ML service with protocol

--- pipelines/services/csv.py
+++ pipelines/services/csv.py
@@ -1,16 +1,19 @@
+from typing import List, Dict, Any, cast
 from dataclasses import dataclass
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
-from pipelines.services.ml_api import MLPredictService
 from pipelines.db.repository import TransactionRepository
 from pipelines.csv.reader import CSVReader
 from pipelines.services.utils import merge_predictions
+from pipelines.services.protocols import MLServiceProtocol
+from pipelines.services.models import TransactionRequest, PredictionResponse
+
 
 @dataclass
 class CSVTransactionIngestService:
     session_factory: async_sessionmaker[AsyncSession]
-    ml_api: MLPredictService
+    ml_api: MLServiceProtocol
     csv_reader: CSVReader
 
     async def run(self) -> int:
@@ -20,10 +23,12 @@ class CSVTransactionIngestService:
             repo = TransactionRepository(session=session)
 
             for chunk in self.csv_reader.read_batches(chunk_size=1000):
-                trx = chunk.to_dict(orient='records')
-                predictions = self.ml_api.predict(trx)
+                trx = cast(List[TransactionRequest], chunk.to_dict(orient="records"))
+                predictions: List[PredictionResponse] = self.ml_api.predict(trx)
                 df = merge_predictions(chunk=chunk, predictions=predictions)
-                affected = await repo.upsert_many(df.to_dict(orient="records"))
+                affected = await repo.upsert_many(
+                    cast(List[Dict[str, Any]], df.to_dict(orient="records"))
+                )
                 await session.commit()
                 total_rows += affected
         return total_rows
