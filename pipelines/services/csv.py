3cb1f123fad2a872d0042a15746dbc576919f29d
Vadim Sokoltsov <vforvad@gmail.com>
2026-01-01 19:39:47 +0100

    Change services classes structure

--- pipelines/services/csv.py
+++ pipelines/services/csv.py
@@ -1,4 +1,8 @@
-from typing import List, Dict, Any, cast
+from abc import ABC, abstractmethod
+import pandas as pd
+from aiokafka import AIOKafkaConsumer
+from aiokafka.structs import ConsumerRecord, TopicPartition
+from typing import List, Dict, Any, cast, AsyncIterator
 from dataclasses import dataclass
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.asyncio.session import AsyncSession
@@ -11,10 +15,12 @@ from pipelines.services.models import TransactionRequest, PredictionResponse
 
 
 @dataclass
-class CSVTransactionIngestService:
+class AbstractTransactionIngestService(ABC):
     session_factory: async_sessionmaker[AsyncSession]
     ml_api: MLServiceProtocol
-    csv_reader: CSVReader
+
+    @abstractmethod
+    def read_batches(self, chunk_size: int = 1000) -> AsyncIterator[pd.DataFrame]: ...
 
     async def run(self) -> int:
         total_rows = 0
@@ -22,7 +28,7 @@ class CSVTransactionIngestService:
         async with self.session_factory() as session:
             repo = TransactionRepository(session=session)
 
-            for chunk in self.csv_reader.read_batches(chunk_size=1000):
+            async for chunk in self.read_batches(chunk_size=1000):
                 trx = cast(List[TransactionRequest], chunk.to_dict(orient="records"))
                 predictions: List[PredictionResponse] = self.ml_api.predict(trx)
                 df = merge_predictions(chunk=chunk, predictions=predictions)
@@ -32,3 +38,27 @@ class CSVTransactionIngestService:
                 await session.commit()
                 total_rows += affected
         return total_rows
+
+
+@dataclass
+class CSVTransactionIngestService(AbstractTransactionIngestService):
+    csv_reader: CSVReader
+
+    async def read_batches(self, chunk_size: int = 1000) -> AsyncIterator[pd.DataFrame]:
+        for chunk in self.csv_reader.read_batches(chunk_size=1000):
+            yield chunk
+
+
+@dataclass
+class KafkaTransactionIngestService(AbstractTransactionIngestService):
+    consumer: AIOKafkaConsumer
+
+    async def read_batches(self, chunk_size: int = 1000) -> AsyncIterator[pd.DataFrame]:
+        batches: Dict[TopicPartition, List[ConsumerRecord]] = (
+            await self.consumer.getmany(timeout_ms=1000, max_records=1000)
+        )
+        for _, records in batches.items():
+            print("CONSUMER RECORDS", records)
+            if records:
+                chunk = pd.DataFrame([r.value for r in records])
+                yield chunk
